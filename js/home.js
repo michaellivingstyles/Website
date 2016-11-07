a9567c7ad7c1521f5bef901fc5ff8b5cddd77bda
michaellivingstyles <michaelitg@163.com>
2016-11-07 11:49:57 +1100

    change mouse over to js

--- js/home.js
+++ js/home.js
@@ -1,19 +1,39 @@
-function showCatalog(index)
+function debugout(txt)
 {
-	var imgid = "shopbycatimg"+index;
-	var img = document.getElementById(imgid);
-	img.style.display = "none";
-	imgid = "shopbycatimgexp"+index;
-	img = document.getElementById(imgid);
-	img.style.display = "block";
+	var dbg = document.getElementById("debug");
+	dbg.innerHTML = txt;
 }
 
-function hideCatalog(index)
-{
-	var imgid = "shopbycatimgexp"+index;
-	var img = document.getElementById(imgid);
-	img.style.display = "none";
-	imgid = "shopbycatimg"+index;
-	img = document.getElementById(imgid);
-	img.style.display = "block";
+function mouseOverCatalog(index)
+{	
+      var divid = "shopbycatimg"+index;
+	var curDiv = document.getElementById(divid);
+	curDiv.className ="shopbycatimgexp";
+	debugout(String(index)+"mouseover");
 }
+function mouseOutCatalog(index)
+{
+	var st = "sbtitle"+index;
+	var stt = document.getElementById(st);
+	var t = String(stt.innerHTML);
+	if( t.length < 17)  //EXPAND
+	{
+		var divid = "shopbycatimg"+index;
+		var curDiv = document.getElementById(divid);
+		curDiv.className ="shopbycatimg";
+	}
+	debugout(String(index)+t.length);
+}
+	
+function clickCatalog(index)
+{
+	var st = "sbtitle"+index;
+	var stt = document.getElementById(st);
+	if( stt.innerHTML == "CLICK TO COLLAPSE"){
+		stt.innerHTML = "CLICK TO EXPAND";
+	}
+	else{
+		stt.innerHTML = "CLICK TO COLLAPSE"
+	}
+}
+
