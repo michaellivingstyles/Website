11a8d3e7ca7a27bd04dbbb7dabf55e3d5e167139
michaellivingstyles <michaelitg@163.com>
2016-11-08 00:38:33 +1100

    click catalogs ok
    ToDo: 3 images group and px change to percentage

--- js/home.js
+++ js/home.js
@@ -60,7 +60,7 @@ function hideCatalog(index)
 		if( i < 3){
 			colDiv.style.marginRight = "1.2821%";
 		}
-		if( i != (index % 3) )  colDiv.style.width = "32.905983%";
+		if( i % 3 != (index % 3) )  colDiv.style.width = "32.905983%";
 	}
 }
 
@@ -76,7 +76,11 @@ function showCatalog(index)
 			var anoDiv = document.getElementById(divid);
 			anoDiv.style.display = "block";
 		}
+		else{
+			curDiv.style.marginTop = "0";
+		}
 		var conDiv = document.getElementById("catalogcontent"+ i);
+		conDiv.innerHTML = "";
 		conDiv.style.display = "none";
 	}
 	for( var i = 1; i <= 3; i++)
@@ -85,7 +89,7 @@ function showCatalog(index)
 		if( i < 3){
 			colDiv.style.marginRight = "2.5640%";
 		}
-		if( i != (index % 3) )  colDiv.style.width = "31.62393%";
+		if( i % 3 != (index % 3) )  colDiv.style.width = "31.62393%";
 	}
 }
 
@@ -96,23 +100,46 @@ function genCatalog(index)
 		curDiv.style.marginBottom = "15px";
 		for( var j = 1; j <= 3; j++)
 		{
-			var con = "";
+			var con1 = "";
+			var con2 = "";
 			for (var i=1; i < 100; i++) {
 				var path = "img/home"+index+"-"+j + "-" + i + ".jpg";
-				var m= getImgMargin(j-1, i-1);
+				var m= getImgMargin(index-1, j-1, i-1);
 				if( m == -1) break;
-				if( m == 0){ con += '<img src="'+path+'" />';}
-			else{ con += '<img style="margin-right:15px;" src = "' + path+ '" />';}
+				if(index < 4 ||  j != index % 3 || i == 1){
+					if( m == 0){ con1 += '<img src="'+path+'" />';}
+					else{ con1 += '<img style="margin-right:15px;" src = "' + path+ '" />';}
+				}
+				else{
+					if( m == 0){ con2 += '<img src="'+path+'" />';}
+					else{ con2 += '<img style="margin-right:15px;" src = "' + path+ '" />';} 
+				}
 			}
 			divid = "catalogcontent" + j; 
 			var conDiv = document.getElementById(divid);
-			conDiv.innerHTML=con;
+			conDiv.innerHTML=con1;
+			if( index > 3 &&  j == index % 3)
+			{
+				divid = "catalogcontent" + index; 
+				var conDiv = document.getElementById(divid);
+				conDiv.innerHTML=con2;
+				if( con1 == ""){
+					var divid = "shopbycatimg"+ index;
+					var curDiv = document.getElementById(divid);
+					curDiv.style.marginTop = "305px";
+				}
+			}
 		}
 }
 
 
-function getImgMargin(col, n) {  
-    var catalogImages1 = [[0,0],[0,1,0,1,0,0,1,0],[1,0,1,0,0]];
-    if( n >= catalogImages1[col].length) return -1; 
-    return catalogImages1[col][n];
+function getImgMargin(index, col, n) {  
+    var catalogImages = [ [[0,0],[0,1,0,1,0,0,1,0],[1,0,1,0,0]],
+    						[[0,0,1,0,1,0,1,0,0],[0,0,0,0],[0,0,1,0,1,0,1,0]],
+    						[[0,0,1,0,0,0],[1,0,1,0,0,1,0,0],[0,0,0]],
+    						[[],[0,1,0,0,1,0],[]],
+    						[[0,0,1,0,0],[0,0,0],[0,0,0,1,0,0]],
+    						[[0,1,0,0,0,0,1,0],[0,1,0,0,1,0],[0]] ];
+    if( n >= catalogImages[index][col].length) return -1; 
+    return catalogImages[index][col][n];
 }
